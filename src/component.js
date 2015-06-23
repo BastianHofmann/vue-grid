b321bd5771a83c00d8158336df2e17952bca1876
Bastian Hofmann <bastianhofmann@me.com>
2015-06-23 20:56:14 +0200

    Moved the pagination to a seperate component, so you can move it somewhere else.

--- src/component.js
+++ src/component.js
@@ -1,50 +1,90 @@
-Vue.component('grid', {
-  replace: true,
-  template: require('./template.html'),
-  props: ['rows', 'columns', 'filter-key', 'per-page'],
-  data: function() {
-    return {
-      rows: null,
-      columns: null,
-      sortColumn: '',
-      filterKey: '',
-      reversed: {},
-      perPage: 10,
-      rawPage: 0
+(function(Vue) {
+
+  var vm = {};
+  var util = {};
+
+  util.paginationData = {};
+
+  util.getPaginationData = function(id) {
+    if(util.paginationData.hasOwnProperty(id)) {
+      return util.paginationData[id];
+    }
+    return util.paginationData[id] = {
+      page: 0,
+      pageCount: 0
     };
-  },
-  computed: {
-    pageCount: function() {
-      return Math.ceil(this.$eval('rows | filterBy filterKey').length / this.perPage);
+  };
+
+  vm.grid = Vue.component('grid', {
+    replace: true,
+    template: require('./template.html'),
+    props: ['rows', 'columns', 'filter-key', 'per-page', 'id'],
+    data: function() {
+      return {
+        rows: [],
+        columns: [],
+        sortColumn: '',
+        filterKey: '',
+        reversed: {},
+        perPage: 10,
+        pagination: null
+      };
     },
-    currentPage: function() {
-      return this.$eval('rows | filterBy filterKey').slice(Math.max(this.page, 0) * this.perPage, Math.max(this.page + 1, 1) * this.perPage);
-    },
-    page: {
-      get: function() {
-        return this.rawPage;
+    computed: {
+      filteredRows: function() {
+        return this.$options.filters.filterBy(this.rows, this.filterKey);
       },
-      set: function(val) {
-        if(val < 0) {
-          this.rawPage = this.pageCount - 1;
-        } else if(val > this.pageCount) {
-          this.rawPage = 1;
-        } else {
-          this.rawPage = val;
+      currentPage: function() {
+        if(this.pagination) {
+          return this.filteredRows.slice(Math.max(this.pagination.page, 0) * this.perPage, Math.max(this.pagination.page + 1, 1) * this.perPage);
+        }
+        return [];
+      }
+    },
+    compiled: function() {
+      this.$data.pagination = util.getPaginationData(this.id);
+      this.$watch(function() {
+        this.pagination.pageCount = Math.ceil(this.filteredRows.length / this.perPage);
+      });
+      var self = this;
+      this.columns.forEach(function(column) {
+        self.reversed.$add(column, false);
+      })
+    },
+    methods: {
+      sortBy: function(column) {
+        this.sortColumn = column;
+        this.reversed[column] = ! this.reversed[column];
+      }
+    }
+  });
+
+  vm.pagination = Vue.component('pagination', {
+    replace: true,
+    template: require('./pagination.html'),
+    props: ['id'],
+    data: function() {
+      return {};
+    },
+    compiled: function() {
+      this.$data = util.getPaginationData(this.id);
+    },
+    methods: {
+      isCurrent: function(index) {
+        return index === this.page;
+      },
+      next: function() {
+        if(this.page < this.pageCount - 1) {
+          this.page++;
+        }
+      },
+      prev: function() {
+        if(this.page > 0) {
+          this.page--;
         }
       }
     }
-  },
-  compiled: function() {
-    var self = this;
-    this.columns.forEach(function(column) {
-      self.reversed.$add(column, false);
-    })
-  },
-  methods: {
-    sortBy: function(column) {
-      this.sortColumn = column;
-      this.reversed[column] = ! this.reversed[column];
-    }
-  }
-});
+  });
+
+
+})(Vue);
